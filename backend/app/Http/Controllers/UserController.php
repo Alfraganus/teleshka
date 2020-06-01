be9f8d815325840a9e8e191c61cec9ab7313d193
Gulomjon Sulaymonov <uzpsychologist@gmail.com>
2020-06-01 15:40:19 +0500

    initial commit

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -5,10 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Models\Telly;
 use App\User;
+use Illuminate\Support\Facades\Auth;
+use Validator;
 
 class UserController extends Controller
 {
-    //
+    public $successStatus = 200;
+
     public function create(Request $request)
     {
         $name = $request->input('fullname');
@@ -20,4 +23,27 @@ class UserController extends Controller
         $model->role=$role;
         $model->save();
     }
+
+    public function register(Request $request)
+    {
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] =  $user->createToken('MyApp')-> accessToken;
+        $success['name'] =  $user->name;
+        return response()->json(['success'=>$success], $this-> successStatus);
+    }
+
+    public function login(){
+        if(Auth::attempt(['fullname' => request('fullname'), 'password' => request('password')])){
+            $user = Auth::user();
+            $success['token'] =  $user->createToken('MyApp')-> accessToken;
+            return response()->json(['success' => $success], $this-> successStatus);
+        }
+        else{
+            return response()->json(['error'=>'Unauthorised'], 401);
+        }
+    }
+
 }
