1fb87b45c999f6195991c9cc0ed9fe1dd4ab0dcb
doniyorbek1993 <65653699+doniyorbek1993@users.noreply.github.com>
2020-06-02 11:46:50 +0500

    new

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('login', 'UserController@login');
-Route::post('register', 'UserController@register');
+Route::post('users/login', 'UserController@login');
+Route::post('users/register', 'UserController@register');
 
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
