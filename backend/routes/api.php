f3d24b4f7445c614c7f2162a707756b01ce5f1bd
doniyorbek1993 <65653699+doniyorbek1993@users.noreply.github.com>
2020-06-03 16:17:18 +0500

    eeeeesssaaawweeesdawerdf

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -18,27 +18,27 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('users/', 'UserController@index');
+Route::get('users', 'UserController@index');
 Route::post('users/create', 'UserController@create');
 Route::post('users/update/{id}','UserController@update');
 Route::delete('users/delete/{id}', 'UserController@destroy');
 
-Route::get('tellies/','TellyController@index');
+Route::get('tellies','TellyController@index');
 Route::post('tellies/create', 'TellyController@create');
 Route::post('tellies/update/{id}','TellyController@update');
 Route::delete('tellies/delete/{id}', 'TellyController@destroy');
 
-Route::get('ppr/' , 'PprController@index');
+Route::get('ppr' , 'PprController@index');
 Route::post('ppr/create', 'PprController@create');
 Route::post('ppr/update/{id}','PprController@update');
 Route::delete('ppr/delete/{id}', 'PprController@destroy');
 
-Route::get('telly-type/', 'TellyTypeController@index');
+Route::get('telly-type', 'TellyTypeController@index');
 Route::post('telly-type/create', 'TellyTypeController@create');
 Route::post('telly-type/update/{id}', 'TellyTypeController@update');
 Route::delete('telly-type/delete/{id}', 'TellyTypeController@destroy');
 
-Route::get('shift/', 'ShiftController@index');
+Route::get('shift', 'ShiftController@index');
 Route::post('shift/create', 'ShiftController@create');
 Route::post('shift/update/{id}', 'ShiftController@update');
 Route::delete('shift/delete/{id}', 'ShiftController@destroy');
