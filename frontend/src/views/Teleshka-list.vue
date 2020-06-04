35c27dc3c4ea920815f5ca87043530118d5cf020
AnvarxuZ <anvarjonkomilov@gmail.com>
2020-06-04 12:53:18 +0500

    cxcxcxc

--- frontend/src/views/Teleshka-list.vue
+++ frontend/src/views/Teleshka-list.vue
@@ -35,7 +35,7 @@
     <v-dialog v-model="saveTellyModal" persistent max-width="450px">
       <v-card>
         <v-card-title>
-          <span class="headline">Yangi teleshka qo'shish</span>
+          <span class="headline">{{ tellyTitle }}</span>
         </v-card-title>
         <v-card-text>
           <v-container>
@@ -115,7 +115,8 @@ export default {
       telly_desc: "",
       addTellyModal: false,
       saveTellyModal: false,
-      form: {}
+      form: {},
+      tellyTitle: ""
     };
   },
   methods: {
@@ -134,6 +135,7 @@ export default {
     },
     newTelly() {
       this.saveTellyModal = true;
+      this.tellyTitle = "Yangi teleshka qo'shish";
       this.form = {
         telly_number: "",
         telly_type_id: "",
@@ -142,6 +144,7 @@ export default {
     },
     editTelly(item) {
       this.saveTellyModal = true;
+      this.tellyTitle = "O'zgartirish";
       this.form = JSON.parse(JSON.stringify(item));
     },
     saveTelly() {
@@ -153,7 +156,14 @@ export default {
           )
           .then(response => {
             this.saveTellyModal = false;
-            this.tellies.push(response.data);
+            Swal.fire({
+              position: "top-end",
+              icon: "success",
+              title: response.data,
+              showConfirmButton: false,
+              timer: 1500
+            });
+            this.getList();
           })
           .catch(function(error) {
             console.log(error);
@@ -168,10 +178,16 @@ export default {
           )
           .then(response => {
             this.saveTellyModal = false;
-            this.tellies = this.tellies.map(v => {
-              if (v.id == response.data.id) v = response.data;
-              return v;
+            Swal.fire({
+              position: "top-end",
+              toast: true,
+              icon: "success",
+              title: response.data,
+              showConfirmButton: false,
+              timer: 1500,
+              timerProgressBar: true
             });
+            this.getList();
           })
           .catch(function(error) {
             console.log(error);
@@ -198,7 +214,16 @@ export default {
             .catch(function(error) {
               console.error(error);
             });
-          Swal.fire("O'chirildi!", "Ushbu teleshka o'chirildi.", "success");
+          Swal.fire({
+            position: "top-end",
+            toast: true,
+            icon: "success",
+            title: "O'chirildi",
+            showConfirmButton: false,
+            width: '250px',
+            timer: 2000,
+            timerProgressBar: true
+          });
         }
       });
     },
--- frontend/src/views/layouts/Login.vue
+++ frontend/src/views/layouts/Login.vue
@@ -25,13 +25,7 @@
                 dense
               ></v-text-field>
               <v-checkbox class="mx-2" label="Remember me" style="margin: 0"></v-checkbox>
-              <v-btn
-                color="rgb(32, 61, 91)"
-                @click="login"
-                :loading="loading"
-                class="col-12"
-                dark
-              >
+              <v-btn color="rgb(32, 61, 91)" @click="login" class="col-12" dark>
                 Login
                 <template v-slot:loader>
                   <span class="custom-loader">
@@ -48,27 +42,31 @@
 </template>
 
 <script>
+import Swal from "sweetalert2";
 const axios = require("axios").default;
+
 export default {
   data() {
     return {
       username: "",
-      password: "",
-      loading: false
+      password: ""
     };
   },
   methods: {
     login() {
-      this.loading = true
+      Swal.fire({
+        title: "Wait ...",
+        onBeforeOpen: () => {
+          Swal.showLoading();
+        }
+      });
       axios
         .post(this.$store.state.backend_url + "/oauth/token", {
           grant_type: "password",
           client_id: "5",
           client_secret: "CZC1cQDoupNtr0J7oX8f4SAuSKZkzCn5BxZZo5aL",
-
           username: this.username,
           password: this.password
-          // scope:
         })
         .then(res => {
           this.$cookies.set(
@@ -77,11 +75,6 @@ export default {
             "1h"
           );
           this.$router.push("/"),
-            // this.$store.dispatch(
-            //   "setAccessToken",
-            //   res.data.token_type + " " + res.data.access_token,
-            //   console.log(res)
-            // );
             (axios.defaults.headers.common = {
               Accept: "application/json",
               "Content-Type": "application/json",
@@ -91,9 +84,17 @@ export default {
             .get(this.$store.state.backend_url + "/api/users/show")
             .then(ress => {
               this.$cookies.set("users", ress.data, "1h");
-              console.log(ress.data);
+              //console.log(ress.data);
             });
-          this.loading = false;
+          Swal.close()
+        })
+        .catch(function(error) {
+          Swal.fire({
+            title: "Login yoki parol xato ..!",
+            icon: "error",
+            confirmButtonColor: "#d33"
+          });
+          console.error(error);
         });
     }
   }
