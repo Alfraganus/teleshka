98cfabd6def9b3a9c81d968245a75738cb4bd5cc
azimjonovshohruh <45550906+azimjonovshohruh@users.noreply.github.com>
2020-06-07 14:09:33 +0500

    vue-charts

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -52,17 +52,57 @@
           </v-card>
         </v-col>
       </v-row>
+      <canvas ref="canvas"></canvas>
     </v-card>
   </div>
 </template>
 
 <script>
+import { Bar } from "vue-chartjs";
 export default {
+  extends: Bar,
   data() {
     return {
       usersLength: "",
       tellysLength: "",
-      pprsLength: ""
+      pprsLength: "",
+      data: {
+        labels: ["Red", "Blue", "Yellow"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [12, 19, 3],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)"
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)"
+            ],
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        }
+      }
     };
   },
   methods: {
@@ -101,6 +141,7 @@ export default {
     this.getUsersCount();
     this.getTellysCount();
     this.getPprCount();
+    this.renderChart(this.data, this.options);
   }
 };
 </script>
